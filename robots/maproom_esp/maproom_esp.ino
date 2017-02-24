a42c83ca528f3a42324fae3ffde4bcc155f8cdef
Chris Anderson <chanderson0@gmail.com>
2017-02-24 15:57:12 -0500

    better maproom esp code

--- robots/maproom_esp/maproom_esp.ino
+++ robots/maproom_esp/maproom_esp.ino
@@ -6,8 +6,12 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-const char* ssid     = "StLouisMaproom";
-const char* password = "CreativeResearch";
+#define MAX_PACKET_LENGTH 1024
+#define HEARTBEAT_TIMEOUT 3000
+#define WIFI_WAIT_TIME    1000
+
+const char *ssid     = "StLouisMaproom";
+const char *password = "CreativeResearch";
 const unsigned int localUdpPort = 5111;
 
 IPAddress remIP(192, 168, 1, 20);
@@ -18,29 +22,9 @@ int bufLen = 0;
 bool bufDone = false;
 
 WiFiUDP udp;
-bool udpUp;
-char incomingPacket[255];
+char incomingPacket[MAX_PACKET_LENGTH];
 
-long lastTime;
-
-void WiFiEvent(WiFiEvent_t event) {
-    bool success;
-    switch(event) {
-        case WIFI_EVENT_STAMODE_GOT_IP:
-            Serial.println("ESCONNECT");
-            udpUp = udp.begin(localUdpPort);
-            if (!udpUp) {
-              Serial.println("ESUDP:FAIL");
-            } else {
-              Serial.printf("ESUDP:%s:%d\n", WiFi.localIP().toString().c_str(), localUdpPort);
-            }
-            break;
-        case WIFI_EVENT_STAMODE_DISCONNECTED:
-            Serial.println("ESDISCONN");
-            udpUp = false;
-            break;
-    }
-}
+long lastHeartbeatTime;
 
 void setup() {
     Serial.begin(19200);
@@ -48,21 +32,34 @@ void setup() {
 
     Serial.println("ESSTART");
 
-    WiFi.setAutoReconnect(true);
-    WiFi.setAutoConnect(true);
-    WiFi.onEvent(WiFiEvent);
     WiFi.begin(ssid, password);
+    WiFi.setAutoReconnect(true);
 
-    lastTime = 0;
+    lastHeartbeatTime = 0;
 }
-
 
 void loop() {
   long now = millis();
 
-  if (now - lastTime > 3000) {
+  if (now - lastHeartbeatTime > HEARTBEAT_TIMEOUT) {
     Serial.println("ESHB");
-    lastTime = now;
+    lastHeartbeatTime = now;
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("ESDISCONN");
+
+    WiFi.reconnect();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(WIFI_WAIT_TIME);
+      Serial.println("ESWAITCONN");
+    }
+
+    Serial.println("ESCONNECT");
+
+    udp.begin(localUdpPort);
+    Serial.printf("ESUDP:%s:%d\n", WiFi.localIP().toString().c_str(), localUdpPort);
   }
 
   while (Serial.available()) {
@@ -89,19 +86,15 @@ void loop() {
       Serial.println("ESRESTART");
       ESP.restart();
     } else if (buf[0] == 'S' && buf[1] == 'E' && buf[2] == 'N') {
-      if (udpUp) {
-        udp.beginPacket(remIP, remPort);
-        udp.write(buf + 3);
-        udp.write('\n');
-        udp.endPacket();
+      udp.beginPacket(remIP, remPort);
+      udp.write(buf + 3);
+      udp.write('\n');
+      udp.endPacket();
 
-        Serial.print("ESSENT");
-        Serial.print(remIP);
-        Serial.print(":");
-        Serial.println(remPort);
-      } else {
-        Serial.println("ESUDPDOWN");
-      }
+      Serial.print("ESSENT");
+      Serial.print(remIP);
+      Serial.print(":");
+      Serial.println(remPort);
     }
 
     bufLen = 0;
@@ -110,7 +103,7 @@ void loop() {
 
   int packetSize = udp.parsePacket();
   if (packetSize) {
-    int len = udp.read(incomingPacket, 255);
+    int len = udp.read(incomingPacket, MAX_PACKET_LENGTH);
     if (len > 0) {
       incomingPacket[len] = 0;
     }
